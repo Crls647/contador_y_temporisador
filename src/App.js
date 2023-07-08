37730ba6183e71ef222f09d18180dcce24d69122
Crls647 <smth647@gmail.com>
2023-07-08 18:39:10 -0400

    primera subida

--- src/App.js
+++ src/App.js
@@ -49,17 +49,18 @@ function App() {
   );
 
 }
-const timeFormat =(date)=>{
-    if(!date) return " 00:00:00 ";
+const timeFormat = (date) => {
+    if (!date) return "00:00:00";
     
     let mm = date.getUTCMinutes();
     let ss = date.getSeconds();
-    let cm = Math.round(date.getMilliseconds()/10);
+    let cm = Math.round(date.getMilliseconds() / 10);
     
     mm = mm < 10 ? "0" + mm : mm;
     ss = ss < 10 ? "0" + ss : ss;
     cm = cm < 10 ? "0" + cm : cm;
-    return `$ {mm}: $ {ss} : $ {cm} `;
+    
+    return `${mm}:${ss}:${cm}`;
 };
 
 export default App;
